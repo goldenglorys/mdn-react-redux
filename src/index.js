0cf06d3b81008e5f1643aa0fead058753a329193
goldenglorys <olusolagloryolamide@gmail.com>
2022-05-04 15:37:29 +0100

    feat: add season detector app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    state = { lat: null, errorMessage: '' };
+    componentDidMount() {
+         window.navigator.geolocation.getCurrentPosition(
+           (position) => this.setState({ lat: position.coords.latitude }),
+           (err) => this.setState({ errorMessage: err.message })
+         );
+    }
+    render() {
+      if(this.state.errorMessage && !this.state.lat){
+        return <div>Error: {this.state.errorMessage}</div>
+      } else if(!this.state.errorMessage && this.state.lat){
+        return <SeasonDisplay lat={this.state.lat}/>
+      } else {
+        return <Spinner message="Please accept location request"/>
+      }
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App/>, document.querySelector('#root'));
