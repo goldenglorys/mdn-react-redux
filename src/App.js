9249e728cffae673943f343a0753982ea4e4e43d
goldenglorys <olusolagloryolamide@gmail.com>
2022-05-09 15:56:58 +0100

    stash wiki

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
     {
@@ -15,7 +16,8 @@ const items = [
 const App = () => {
   return (
     <div>
-      <Accordion items={items}/>
+      {/* <Accordion items={items}/> */}
+      <Search/>
     </div>
   );
 };
