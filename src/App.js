6a549685997ca4ffd07f1e1baad6694a50ce165c
goldenglorys <olusolagloryolamide@gmail.com>
2022-05-17 17:40:49 +0100

    feat: implement useref hook in fnc component(Dropdown)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
-const items = [
-    {
-        title: 'What is React?',
-        content: 'React is a front end javascrJSipt library'
-    },
-    {
-        title: 'Why use React?',
-        content: 'React is a favorite JS library among engineers'
-    }
+const options = [
+  {
+    label: "The Color Cyan",
+    value: "cyan",
+  },
+  {
+    label: "The Color Lemon",
+    value: "lemon",
+  },
+  {
+    label: "The Color Navy Blue",
+    value: "navy-blue",
+  },
 ];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
       {/* <Accordion items={items}/> */}
-      <Search/>
+      {/* <Search/> */}
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
     </div>
   );
 };
