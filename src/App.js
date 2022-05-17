5aef90f3a5dc46cba17a36ac17a0b26925600036
goldenglorys <olusolagloryolamide@gmail.com>
2022-05-17 19:39:54 +0100

    feat: implement header navigation without react-router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Traanslate";
+import Header from "./components/Header";
+import Route from "./components/Route";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascrJSipt library",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+];
 
 const options = [
   {
@@ -20,17 +33,26 @@ const options = [
 ];
 
 const App = () => {
-  // const [selected, setSelected] = useState(options[0]);
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      {/* <Accordion items={items}/> */}
-      {/* <Search/> */}
-      {/* <Dropdown
-        options={options}
-        selected={selected}
-        onSelectedChange={setSelected}
-      /> */}
-      <Translate/>
+      <Header/>
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
---
+++ src/components/Link.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Link = ({ className, href, children }) => {
+  const onClick = (event) => {
+    event.preventDefault();
+    window.history.pushState({}, "", href);
+
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
+  };
+  return (
+    <a className={className} href={href} onClick={onClick}>
+      {children}
+    </a>
+  );
+};
+
+export default Link;
